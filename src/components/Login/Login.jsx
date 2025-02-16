dbdb78bd3a2ca8444b0faa9a0e1826814074eba0
Prathamesh Kulkarni <prathameshkulkarni22092004@gmail.com>
2025-02-16 23:49:54 +0530

    second commit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
-import { Outlet } from 'react-router'
+import { Link, Links, Outlet } from 'react-router'
+
+
+
+
 
 function Login() {
+
+  const handleLogin = () => {
+    // document.querySelector('#selectlogin').classList.add('hidden')
+  }
+
+
   return (
-    <div>
-      <div>
-        Select user type
+    <>
+    <div id='selectlogin' className=''>
+      <div className='my-15'>
+        <h1 className='text-5xl text-center text-red-800 underline '>Select user type</h1>
       </div>
-      <Outlet/>
+      <div className='flex justify-center gap-3'>
+        <Link to={'/login/loginowner'} onClick={handleLogin}>
+          <div className='flex justify-center bg-amber-100 w-50 p-3 rounded-2xl'>
+            <button className='text-center'>OWNER</button>
+          </div>
+        </Link>
+        <Link to={'/login/loginstudent'} onClick={handleLogin}>
+        <div className='flex justify-center bg-amber-100 w-50 p-3 rounded-2xl'>
+          <button className=''>STUDENT</button>
+        </div>
+        </Link>
+      </div>
     </div>
+    <Outlet />
+    </>
   )
 }
 
--- src/components/Login/LoginOwner.jsx
+++ src/components/Login/LoginOwner.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function LoginOwner() {
   return (
-    <div>
-        LoginOwner
+    <div className='flex justify-center items-center bg-white text-black h-dvh w-full'>
+        <div className='border-2 border-black w-[400px] h-[300px]'>
+
+        </div>
     </div>
   )
 }
